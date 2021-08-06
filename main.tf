9439c5077b0554b1ceda9979e1698d91364b19c2
MT <mtrojas@protonmail.com>
2021-08-06 04:48:01 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "server" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
