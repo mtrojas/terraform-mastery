2dbd8b61217a77c70078c9849ce451889356b1b3
MT <mtrojas@protonmail.com>
2021-08-07 11:08:10 -0400

    feat: provision a webserver with user_data

--- main.tf
+++ main.tf
@@ -3,10 +3,27 @@ provider "aws" {
 }
 
 resource "aws_instance" "server" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  user_data              = <<-EOF
+  #!/bin/bash
+  echo "Hello, World" > index.html
+  nohup busybox httpd -f -p 8080 &
+  EOF
 
   tags = {
-    Name = "terraform-example"
+    Name = "web-server"
+  }
+}
+
+resource "aws_security_group" "sg" {
+  name = "secgroup-web-server"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
