897eae9d074becb64e1118d07452f12cac32f92e
MT <mtrojas@protonmail.com>
2021-08-12 10:21:08 -0400

    fix: add config to destroy without final snapshot

--- stage/data-stores/mysql/main.tf
+++ stage/data-stores/mysql/main.tf
@@ -16,6 +16,9 @@ resource "aws_db_instance" "db" {
   instance_class    = "db.t2.micro"
   name              = "test"
   username          = "admin"
+  # Using the following config to destroy without a final snapshot
+  skip_final_snapshot = true
+  apply_immediately   = true
   # Could not make it work with AWS Secrets Manager, every time got weird errors regarding the password stored:
   # Error: Error creating DB Instance: InvalidParameterValue: The parameter MasterUserPassword is not a valid password. Only printable ASCII characters besides '/', '@', '"', ' ' may be used.
   # Error: Error creating DB Instance: InvalidParameterValue: The parameter MasterUserPassword is not a valid password because it is longer than 41 characters.
