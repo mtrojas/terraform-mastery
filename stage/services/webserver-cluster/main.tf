d56e286f766be76e78c96e325ba25bf57e457f1c
MT <mtrojas@protonmail.com>
2021-08-13 06:20:20 -0400

    chore: access child module output

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -15,3 +15,9 @@ module "webserver_cluster" {
   db_remote_state_bucket = "terraform-mastery-remote-backend"
   db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
 }
+
+# Accessing Child Module Outputs
+output "alb_dns_name" {
+  value       = module.webserver_cluster.alb_dns_name
+  description = "The domain name of the load balancer"
+}
