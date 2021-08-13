8d549d14333cfa7479accadaf25e54bf2d2cbd89
MT <mtrojas@protonmail.com>
2021-08-13 05:43:02 -0400

    feat: set the input variables values in staging

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -11,4 +11,7 @@ provider "aws" {
 module "webserver_cluster" {
   source = "../../../modules/services/webserver-cluster"
 
+  cluster_name           = "webservers-stage"
+  db_remote_state_bucket = "terraform-mastery-remote-backend"
+  db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
 }
