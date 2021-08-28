6fa2f656f0c7d3ab52953627535a02f4836f6d91
MT <mtrojas@protonmail.com>
2021-08-28 06:43:36 -0400

    chore: modify the stage mysql code to use the module

--- stage/data-stores/mysql/main.tf
+++ stage/data-stores/mysql/main.tf
@@ -4,26 +4,26 @@ terraform {
   }
 }
 
-
 provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_db_instance" "db" {
-  identifier_prefix = "db-servers"
-  engine            = "mysql"
-  allocated_storage = 10
-  instance_class    = "db.t2.micro"
-  name              = "test"
-  username          = "admin"
-  # Using the following config to destroy without a final snapshot
-  skip_final_snapshot = true
-  apply_immediately   = true
-  # Could not make it work with AWS Secrets Manager, every time got weird errors regarding the password stored:
-  # Error: Error creating DB Instance: InvalidParameterValue: The parameter MasterUserPassword is not a valid password. Only printable ASCII characters besides '/', '@', '"', ' ' may be used.
-  # Error: Error creating DB Instance: InvalidParameterValue: The parameter MasterUserPassword is not a valid password because it is longer than 41 characters.
-  # Ended up using the environment variable TF_VAR
-  password = var.db_password
+# Passing the variable from the child module
+variable "db_password" {}
+
+module "mysql" {
+  source = "../../../modules/data-stores/mysql"
+
+  cluster_name = "stage"
+  db_password  = var.db_password
+
 }
 
+# Accessing Child Module Outputs
+output "address" {
+  value = module.mysql.address
+}
 
+output "port" {
+  value = module.mysql.address
+}
