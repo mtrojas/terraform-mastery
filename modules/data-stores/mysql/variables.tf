fb082bde350f20f366cb0505c5a08a0f93db4943
MT <mtrojas@protonmail.com>
2021-08-28 15:27:22 -0400

    refactor: move modules to a new repository

--- modules/data-stores/mysql/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "db_password" {
-  description = "The password for the database"
-  type        = string
-}
-
-variable "cluster_name" {
-  description = "The name to use for all the cluster resources"
-  type        = string
-}
--- modules/services/webserver-cluster/user-data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cat > index.html <<EOF
-<h1>Hello, World!</h1>
-<p>Database Address: ${db_address}</p>
-<p>Database Port: ${db_port}</p>
-EOF
-
-nohup busybox httpd -f -p ${server_port} &
