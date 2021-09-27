25b5cc1a9ec046e6397e79b29f098bb510235045
MT <mtrojas@protonmail.com>
2021-09-27 07:09:16 -0300

    chore: update stage configuration to use autoscaling from module

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -9,14 +9,21 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source = "github.com/mtrojas/terraform-mastery-modules//services/webserver-cluster?ref=v0.0.1"
+  source = "github.com/mtrojas/terraform-mastery-modules//services/webserver-cluster?ref=v0.0.3"
 
   cluster_name           = "webservers-stage"
   db_remote_state_bucket = "terraform-mastery-remote-backend"
   db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
 
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-  min_size      = 2
-  max_size      = 2
+  ami                = "ami-0c55b159cbfafe1f0"
+  instance_type      = "t2.micro"
+  min_size           = 2
+  max_size           = 2
+  enable_autoscaling = false
+
+  custom_tags = {
+    Owner       = "team-devops"
+    DeployedBy  = "terraform"
+    Environment = "staging"
+  }
 }
