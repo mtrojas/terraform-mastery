5952678d0e7786894d4b2ad5142535d69297c6ae
MT <mtrojas@protonmail.com>
2021-09-27 07:08:49 -0300

    chore: update prod configuration to use autoscaling from module

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -9,40 +9,22 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source = "github.com/mtrojas/terraform-mastery-modules//services/webserver-cluster?ref=v0.0.2"
+  source = "github.com/mtrojas/terraform-mastery-modules//services/webserver-cluster?ref=v0.0.3"
 
   cluster_name           = "webservers-prod"
   db_remote_state_bucket = "terraform-mastery-remote-backend"
   db_remote_state_key    = "prod/data-stores/mysql/terraform.tfstate"
 
-  ami           = "ami-0747bdcabd34c712a"
-  instance_type = "m4.large"
-  min_size      = 2
-  max_size      = 10
+  ami                = "ami-0747bdcabd34c712a"
+  instance_type      = "m4.large"
+  min_size           = 2
+  max_size           = 10
+  enable_autoscaling = true
 
   custom_tags = {
-    Owner      = "team-devops"
-    DeployedBy = "terraform"
+    Owner       = "team-devops"
+    DeployedBy  = "terraform"
+    Environment = "production"
   }
-}
-
-resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
-  scheduled_action_name = "scale-out-during-business-hours"
-  min_size              = 2
-  max_size              = 10
-  desired_capacity      = 10
-  recurrence            = "0 9 * * *"
-
-  autoscaling_group_name = module.webserver_cluster.asg_name
-}
-
-resource "aws_autoscaling_schedule" "scale_in_at_night" {
-  scheduled_action_name = "scale-in-at-night"
-  min_size              = 2
-  max_size              = 10
-  desired_capacity      = 2
-  recurrence            = "0 17 * * *"
-
-  autoscaling_group_name = module.webserver_cluster.asg_name
 }
 
--- prod/services/webserver-cluster/outputs.tf
+++ prod/services/webserver-cluster/outputs.tf
@@ -3,8 +3,3 @@ output "alb_dns_name" {
   value       = module.webserver_cluster.alb_dns_name
   description = "The domain name of the load balancer"
 }
-
-output "asg_name" {
-  value       = module.webserver_cluster.asg_name
-  description = "The name of the Auto Scaling Group"
-}
