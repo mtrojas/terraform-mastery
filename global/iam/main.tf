550cf25ee4b8478bf0d4023c6257efa820fa2750
MT <mtrojas@protonmail.com>
2021-10-01 22:54:36 +0100

    chore: conditionally attach iam policy to user neo

--- global/iam/main.tf
+++ global/iam/main.tf
@@ -13,4 +13,50 @@ resource "aws_iam_user" "tf_users" {
   name     = each.value
 }
 
+# IAM Policy that allow read-only access to CloudWatch
+resource "aws_iam_policy" "cloudwatch_read_only" {
+  name   = "cloudwatch-read-only"
+  policy = data.aws_iam_policy_document.cloudwatch_read_only.json
+}
+
+resource "aws_iam_user_policy_attachment" "neo_cloudwatch_read_only" {
+  count = var.give_neo_cloudwatch_full_access ? 0 : 1
+
+  user       = aws_iam_user.tf_users["neo"].name
+  policy_arn = aws_iam_policy.cloudwatch_read_only.arn
+}
+
+data "aws_iam_policy_document" "cloudwatch_read_only" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "cloudwatch:Describe*",
+      "cloudwatch:Get*",
+      "cloudwatch:List*"
+    ]
+    resources = ["*"]
+  }
+}
+
+# IAM Policy that allows full (read and write) access to CloudWatch
+resource "aws_iam_policy" "cloudwatch_full_access" {
+  name   = "cloudwatch-full-access"
+  policy = data.aws_iam_policy_document.cloudwatch_full_access.json
+}
+
+resource "aws_iam_user_policy_attachment" "neo_cloudwatch_full_access" {
+  count = var.give_neo_cloudwatch_full_access ? 1 : 0
+
+  user       = aws_iam_user.tf_users["neo"].name
+  policy_arn = aws_iam_policy.cloudwatch_full_access.arn
+
+}
+
+data "aws_iam_policy_document" "cloudwatch_full_access" {
+  statement {
+    effect    = "Allow"
+    actions   = ["cloudwatch:*"]
+    resources = ["*"]
+  }
+}
 
--- global/iam/variables.tf
+++ global/iam/variables.tf
@@ -1,7 +1,12 @@
 variable "user_names" {
   description = "Create IAM users with these names"
   type        = list(string)
-  default     = ["neo", "morpheus"]
+  default     = ["neo", "trinity", "morpheus"]
+}
+
+variable "give_neo_cloudwatch_full_access" {
+  description = "If true, neo gets full access to CloudWatch"
+  type        = bool
 }
 
 variable "family_names" {
