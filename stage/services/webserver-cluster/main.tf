4722375b0db2c642f49a16a6d2ee180555789e0c
MT <mtrojas@protonmail.com>
2021-10-03 12:46:33 +0100

    chore: update to use new module v0.0.6 version and add variable

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -9,17 +9,18 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source = "github.com/mtrojas/terraform-mastery-modules//services/webserver-cluster?ref=v0.0.5"
+  source = "github.com/mtrojas/terraform-mastery-modules//services/webserver-cluster?ref=v0.0.6"
 
   cluster_name           = "webservers-stage"
   db_remote_state_bucket = "terraform-mastery-remote-backend"
   db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
 
-  ami                = "ami-0c55b159cbfafe1f0"
-  instance_type      = "t2.micro"
-  min_size           = 2
-  max_size           = 2
-  enable_autoscaling = false
+  ami                  = "ami-0c55b159cbfafe1f0"
+  instance_type        = "t2.micro"
+  min_size             = 2
+  max_size             = 2
+  enable_autoscaling   = false
+  enable_new_user_data = true
 
   custom_tags = {
     Owner       = "team-devops"
