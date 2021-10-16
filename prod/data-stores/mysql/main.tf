deba690fb9e2f1d4760a79ddf75bf0445a191db9
MT <mtrojas@protonmail.com>
2021-10-16 18:19:22 +0100

    feat: use the prod mysql with new module version

--- prod/data-stores/mysql/main.tf
+++ prod/data-stores/mysql/main.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_providers {
+    # Allow any 3.x version of the AWS provider
+    aws = {
+      version = "~> 3.0"
+    }
+  }
+
   backend "s3" {
     key = "prod/data-stores/mysql/terraform.tfstate"
   }
@@ -8,22 +15,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Passing the variable from the child module
-variable "db_password" {}
-
 module "mysql" {
-  source = "github.com/mtrojas/terraform-mastery-modules//data-stores/mysql?ref=v0.0.1"
+  source = "github.com/mtrojas/terraform-mastery-modules//data-stores/mysql?ref=v0.0.12"
 
-  cluster_name = "prod"
-  db_password  = var.db_password
+  db_name     = var.db_name
+  db_username = var.db_username
+  db_password = var.db_password
 
-}
-
-# Accessing Child Module Outputs
-output "address" {
-  value = module.mysql.address
-}
-
-output "port" {
-  value = module.mysql.port
 }
